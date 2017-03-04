7b29edebdb6e8c38ed7899ec0cf8c3b07c29a3bc
jdavidagudelo <jdaaa2009@gmail.com>
2017-03-04 16:49:59 -0500

    Initial.

---
+++ examples/class_1.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+scores = [3, 1.0, 0.2]
+
+
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    return np.exp(x) / np.sum(np.exp(x), axis=0)
+
+
+print(softmax(scores))
+
+x = np.arange(-2.0, 6.0, 0.1)
+scores = np.vstack([x, np.ones_like(x), 0.2 * np.ones_like(x)])
+
+plt.plot(x, softmax(scores).T, linewidth=2)
+plt.show()
